53a40430a136401da304048700b4d20ea5cad167
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-01-30 14:39:26 -0600

    h2 - Nearly Jumping
    Started working on the much better jumping - it works, but I'm missing a piece of it; it applies a force perpendicular to the dimension, but I have yet to make sure it's not INTO THE RAIL. So sometimes it works, sometimes it's just bashing itself into the floor.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,41 +1,63 @@
 class_name Player extends CharacterBody2D
 # The class for the player. Snaps(?) to the nearest rail.
 
-@export var grind_speed := 500
+@export var grind_speed := 700.0
+@export var jump_height := 700.0
+
+const JUMP_BUFFER := 0.1
+var jump_buffering := 0.0
+
+const SNAP_BUFFER := 0.1
+var snap_buffering := 0.0
 
 var snapped_to:Rail
-var snap_queue:Array[Rail]
 
+# A normalized vector of the direction the player is grinding in.
 var direction:Vector2 : set =_set_dir
-
 func _set_dir(to:Vector2): direction = to.normalized()
 
-func _ready():
-	direction = Vector2.DOWN#.slide()
-
 func _physics_process(delta: float) -> void:
+	# For the line debugging.
+	queue_redraw()
 	
-	if snapped_to: velocity = direction * grind_speed
-	else:
+	jump_buffering = move_toward(jump_buffering, 0, delta)
+	if Input.is_action_just_pressed("Jump"): jump_buffering = JUMP_BUFFER
+	
+	snap_buffering = move_toward(snap_buffering, 0, delta)
+	
+	# Grinding
+	if snapped_to and not snap_buffering: 
+		velocity = direction * grind_speed
+		
+		if jump_buffering: # Jump
+			
+			print("YUMP")
+			
+			velocity += Vector2(0, jump_height).rotated(direction.angle())
+			
+			snap_buffering = SNAP_BUFFER
+			
+			jump_buffering = 0.0
+	
+	# Freefall
+	else: 
+		#rotation += 0.1
+		
 		velocity += get_gravity() * delta
+		
+		# Set the current direction to the velocity (automatically normalized).
+		direction = velocity
 	
 	move_and_slide()
-	pass
-
-#func _process(delta: float) -> void:
-	#if not snap_queue.has(snapped_to): snapped_to = null
-	#if not snap_queue.is_empty() and not snapped_to:
-		#snap_to(snap_queue[0])
 
 func _on_rail_enter(rail:Node2D): if rail is Rail: snap_to(rail)
 func _on_rail_exit (rail:Node2D): if rail is Rail: if snapped_to == rail: snapped_to = null
 
 func snap_to(rail:Rail):
-	print("snapped to ", rail)
 	snapped_to = rail
 	
 	# Create a vector rotated to mimic the rail's rotation.
-	var rail_vector = Vector2.ONE.rotated(rail.rotation)
+	var rail_vector = Vector2.DOWN.rotated(rail.rotation)
 	
 	# Find the two vectors parallel to the rail.
 	var a = rail_vector.rotated(deg_to_rad(-90))
@@ -46,3 +68,12 @@ func snap_to(rail:Rail):
 	var b_dot = b.dot(direction)
 	
 	direction = a if a_dot > b_dot else b
+
+## DEBUG
+func _draw() -> void:
+	# Debug lines to show the direction and plane parallel. NOTE: Doesn't show correctly with rotation.
+	draw_line(Vector2.ZERO, -direction * 250, Color.RED, 15)
+	draw_line(Vector2.ZERO, Vector2(0, -jump_height).rotated(direction.angle()), Color.BLUE, 15)
+	
+	if snapped_to:
+		draw_line(to_local(snapped_to.global_position), (Vector2.DOWN.rotated(snapped_to.rotation) * 250) + to_local(snapped_to.global_position), Color.GREEN, 15)
