67678371ebe7e6afa4b51488663a8cbd715b833b
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-02-03 14:36:03 -0600

    h11.4 - Animation Overlay
    Made an animation overlay for the transition between the levels screen and the actual.

---
+++ Scripts/AnimationOverlay.gd
@@ -0,0 +1,23 @@
+@tool
+class_name AnimationOverlay extends Control
+
+# A float displaying the amount of progress in h
+@export var progress := 0.0 : set = set_progress
+func set_progress(to:float): progress = clamp(to, 0.0, 1.0)
+
+var rects:Array[ColorRect]
+var rect_seeds:Array[float]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	for child in get_children(): if child is ColorRect: 
+		rects.append(child)
+		rect_seeds.append(randf_range(-2, -2.25))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	for i in range(len(rects)):
+		rects[i].position.x = (ease(progress, rect_seeds[i]) - 0.63) * 3500
+	
+	pass
