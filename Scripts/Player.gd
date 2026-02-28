28fc8f770c528e8ab0d6b94c5810ddac19c90c4d
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2026-02-27 19:48:59 -0600

    h20.3 - Player Cleanup
    Cleaned up all the obsolete code in the player's script as I try to get it generalized to all surfaces. Next, I have a bit more work to do on splines, then we'll come back to this.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,7 @@
 class_name Player extends CharacterBody2D
 # The class for the player. Snaps(?) to the nearest rail.
 
-const DEBUG := true
+const DEBUG := false#true
 
 @onready var spawn_position = global_position
 
@@ -27,7 +27,16 @@ func _physics_process(delta: float) -> void:
 	if DEBUG: queue_redraw()
 	
 	
+	# Buffer the jump input.
+	jump_buffering = move_toward(jump_buffering, 0, delta)
+	if Input.is_action_just_pressed("Jump"): jump_buffering = JUMP_BUFFER
+	
+	
+	# Grinding
 	if is_on_wall():
+		
+		## -- DIRECTION SETTING -- ##
+		
 		# Create a vector rotated to mimic the rail's rotation.
 		var grind_vector = get_wall_normal()
 		
@@ -37,40 +46,20 @@ func _physics_process(delta: float) -> void:
 		
 		# Figure out which direction is closer to the current direction, and set to that.
 		direction = closest([a,b], direction) - (grind_vector / 2)
-		#print(direction, " out of ", [a,b], " from ", get_wall_normal())
 		
-		#on_snap_mag = mag(velocity.project(direction))
-	
-	
-	# Buffer the jump input.
-	jump_buffering = move_toward(jump_buffering, 0, delta)
-	if Input.is_action_just_pressed("Jump"): jump_buffering = JUMP_BUFFER
-	
-	
-	# Grinding
-	if is_on_wall():
+		## -- VELOCITY APPLICATION -- ##
+		
 		velocity = direction * max(grind_speed, on_snap_mag)
 		
 		
-		
-		if jump_buffering: # Jump
+		## -- JUMPING -- ##
+		if jump_buffering:
 			
-			# Get the two options for jump directions. (Perpendiculars to current dir).
-			var a = direction.rotated(deg_to_rad(90))
-			var b = direction.rotated(deg_to_rad(-90))
-			
-			# Compare them to the direction from the rail to the player.
-			var rail_to_player = get_wall_normal()
-			
-			# Choose the jump direction based on which is closer to that direction.
-			var jump_direction = get_wall_normal()
-			
-			velocity += jump_direction * mag(velocity)
+			# Jump completely perpendicular to the current surface,
+			# with all the current velocity.
+			velocity += get_wall_normal() * mag(velocity)
 			
 			jump_buffering = 0.0
-			
-			snapped_to = null
-			
 	
 	# Freefall
 	else:
@@ -88,27 +77,6 @@ func _physics_process(delta: float) -> void:
 	if not DEBUG: rotation += deg_to_rad(sqrt(pow(velocity.x,2) + pow(velocity.y, 2)) / 40)
 	
 	move_and_slide()
-
-func _on_rail_enter(rail:Node2D): 
-	if   rail is DeathRail: Global.reset_level.emit() # Die.
-	elif rail is Rail: snap_to(rail) # Snap to this rail.
-func _on_rail_exit (rail:Node2D): if snapped_to == rail: snapped_to = null
-
-func snap_to(rail:Rail): pass
-	#snapped_to = rail
-	#
-	#
-	## Create a vector rotated to mimic the rail's rotation.
-	#var rail_vector = Vector2.DOWN.rotated(rail.rotation)
-	#
-	## Find the two vectors parallel to the rail.
-	#var a = rail_vector.rotated(deg_to_rad(-90))
-	#var b = rail_vector.rotated(deg_to_rad(90))
-	#
-	## Figure out which direction is closer to the current direction, and set to that.
-	#direction = closest([a,b], direction)
-	#
-	#on_snap_mag = mag(velocity.project(direction))
 
 # Returns the Vector2 that is most similar to the comparator out of the given array.
 func closest(of:Array[Vector2], compared_to:Vector2):
